1a78c946a95c5acb7b53aa0652553dc63d3a8b77
Wietse De Koninck <65445401+H-Finch-404@users.noreply.github.com>
2023-05-16 19:08:04 +0200

    Remove presentation layer folders that are not used
    [-] Regular API Controllers
    [-] MVC Controllers
    [-] Pages
    [-] ViewModels
    [-] ApiModels
    [-] Views
    [-] Related tests

--- src/CleanArchitectureArchUnit.Web/ViewModels/ErrorViewModel.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace CleanArchitectureArchUnit.Web.ViewModels;
-
-public class ErrorViewModel
-{
-  public string? RequestId { get; set; }
-
-  public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-}
