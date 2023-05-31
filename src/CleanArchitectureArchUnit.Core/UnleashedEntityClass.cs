6f34da7d9562d0cf2a271bbb94b1e3cf6a711ee8
H-Finch-404 <65445401+H-Finch-404@users.noreply.github.com>
2023-05-31 15:50:02 +0200

    Renames

---
+++ src/CleanArchitectureArchUnit.Core/UnleashedEntityClass.cs
@@ -0,0 +1,15 @@
+﻿using CleanArchitectureArchUnit.SharedKernel;
+using CleanArchitectureArchUnit.SharedKernel.Interfaces;
+
+namespace CleanArchitectureArchUnit.Core;
+
+// This class will violate the test/architectural rule
+// AggregateRoots_Should_Reside_InCoreLayer_And_AggregatesNamespace
+
+public class UnleashedEntityClass :  EntityBase, IAggregateRoot
+{ 
+  public UnleashedEntityClass()
+  {
+   
+  }
+}
