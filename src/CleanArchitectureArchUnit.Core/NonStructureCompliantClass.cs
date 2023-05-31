a580e5350e05f82442b315f7401c9c093da3cc18
H-Finch-404 <65445401+H-Finch-404@users.noreply.github.com>
2023-05-31 15:11:52 +0200

    Comment the non structure-compliant class

--- src/CleanArchitectureArchUnit.Core/NonStructureCompliantClass.cs
+++ src/CleanArchitectureArchUnit.Core/NonStructureCompliantClass.cs
@@ -1,10 +1,10 @@
-﻿using CleanArchitectureArchUnit.SharedKernel.Interfaces;
-
-namespace CleanArchitectureArchUnit.Core;
-
-// This class will violate the AggregateRoots_Should_Reside_InCoreLayer_And_AggregatesNamespace test
-
-public class NonStructureCompliantClass : IAggregateRoot
-{
-  
-}
+﻿// using CleanArchitectureArchUnit.SharedKernel.Interfaces;
+//
+// namespace CleanArchitectureArchUnit.Core;
+//
+// // This class will violate the AggregateRoots_Should_Reside_InCoreLayer_And_AggregatesNamespace test
+//
+// public class NonStructureCompliantClass : IAggregateRoot
+// {
+//   
+// }
